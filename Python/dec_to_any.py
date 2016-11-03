51cc1e6266b146a993834f0c85a69584c909d7dd
RAHUL RAJ <rahul7.raj9@gmail.com>
2016-11-03 11:40:57 -0400

    Update dec_to_any.py
    Updated to convert decimal to any base between (2-36)

--- Python/dec_to_any.py
+++ Python/dec_to_any.py
@@ -2,11 +2,12 @@
 
 def dectoAny(num, base):
 	stack=[]
-	digits = "0123456789ABCDEF"
+	digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	if not 1 < base < 37:
+		raise ValueError("base must be between 2 and 36")
 	while(num>0):
 		rem=num%base
 		num=num//base ##returns int 
 		stack.append(str(digits[rem]))
 	return (''.join(stack[::-1]))
-print (dectoAny(255,16))
-
+print (dectoAny(61,17))
